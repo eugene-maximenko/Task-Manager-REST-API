f7788cee174bbb399756d51adc300388f0418302
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-05-25 22:07:47 +0300

    Added read database function

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.post('/users', (req, res) => {
     });
 })
 
-app.get('/users', (req, res) => {
+app.get('/users/', (req, res) => {
     User.find({}).then((users) => {
         res.send(users);
     }).catch((e) => {
@@ -26,8 +26,18 @@ app.get('/users', (req, res) => {
     })
 })
 
-app.get('/users', (req, res) => {
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
 
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
 })
 
 app.post('/tasks', (req, res) => {
