e0c75908c8aa41530efd79b14d1f55e8d285e0e4
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-05-28 22:53:44 +0300

    New patch function for tasks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 const { updateOne } = require('./models/user');
+const { ObjectID } = require('bson');
 
 const app = express();
 const port = process.env.port || 3000;
@@ -51,8 +52,8 @@ app.get('/users/:id', async (req, res) => {
 
 app.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body);
-    const allowedUpdated = ['name', 'email', 'password', 'age'];
-    const isValidOperation = updates.every(update => allowedUpdated.includes(update));
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
@@ -109,8 +110,31 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e);
+    }
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
 
 console.log('Here i am');
