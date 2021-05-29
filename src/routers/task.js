ff696eab8a7163cba427d771226f9549d9d9de64
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-05-29 15:21:06 +0300

    Prepared tasks update function before adding middleware

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const Task = require('../models/task');
- 
+
 router.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
@@ -14,7 +14,6 @@ router.post('/tasks', async (req, res) => {
 })
 
 router.get('/tasks', async (req, res) => {
-
 
     try {
         const tasks = await Task.find({});
@@ -50,7 +49,10 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        const task = await Task.findById(req.params.id);
+
+        updates.forEach(update => task[update] = req.body[update]);
+        await task.save();
 
         if (!task) {
             return res.status(404).send();
