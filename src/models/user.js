fecf899b4838ab95d08c4f2def3ad30aa816283f
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-01 21:13:33 +0300

    Setting up a generation of json web token

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -41,6 +42,11 @@ const userSchema = new mongoose.Schema({
         }
     }
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse');
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     // Find the user by the email
