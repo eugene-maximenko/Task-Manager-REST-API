8910ed450051746007b1e00bc1662acf9a995721
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-01 23:25:11 +0300

    Messed up with middleware for routes

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.port || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === "GET") {
+//         res.send('GET requests are disabled');
+//     } else {
+//         next();
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('The site is under maintenance, please try back soon.');
+})
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
+
 
 
 app.listen(port, () => {
