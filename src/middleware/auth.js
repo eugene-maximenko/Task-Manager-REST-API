f8c4de873cc6d89bbd30c4a8aa481251f2b2a38d
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-02 19:52:38 +0300

    Added 1 middleware for users search function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,8 +5,16 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'thisismynewcourse');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.tokenz': token });
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error();
+        }
+
+        req.user = user;
+        next();
     } catch (e) {
+        console.log(e);
         res.status(401).send({ error: 'Please authenticate.' });
     }
 }
