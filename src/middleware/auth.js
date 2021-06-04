e6995d4651d2e11ce078fad0ccd8f34cde96b31c
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-04 10:11:33 +0300

    Added some comments to login middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,6 +5,7 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'thisismynewcourse');
+        // The code below parses the document tokens array to find a token
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
         if (!user) {
