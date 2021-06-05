10e2bbee1b50c63a998f22d9aa40ea466720505f
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-05 11:15:25 +0300

    Updated task delete func

--- src/routers/task.js
+++ src/routers/task.js
@@ -54,8 +54,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({ _id: req.params._id, owner: req.user._id })
-
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send();
@@ -70,10 +69,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
 
     try {
-        const task = await Task.findByIdAndDelete(req.params.id);
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
         if (!task) {
             return res.status(404).send();
