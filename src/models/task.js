c9990ba739bdd92eb08ef4f59c23f059bc2e0e59
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-06 23:38:31 +0300

    Used mongoose Schema for task model

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const Task = mongoose.model('Task', {
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     owner: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         required: true,
+//         ref: 'User'
+//     }
+// });
+
+const taskSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true,
@@ -16,6 +33,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
-});
+}, {
+    timestamps: true
+})
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
