b6082f6b19f2ebaf2ecadc03db2b07e69bab0a01
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-08 00:05:34 +0300

    Set up endpoint for uploading avatars v.1

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
 
+const upload = multer({
+    dest: 'avatars'
+});
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -82,6 +86,10 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send();
     }
 })
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send();
+});
 
 module.exports = router;
 
