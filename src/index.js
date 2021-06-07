89bfe4752ad38070f5f2494fc6e4cf05bc3997d8
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-07 23:49:03 +0300

    Added a file uploading router

--- src/index.js
+++ src/index.js
@@ -7,17 +7,15 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.port || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === "GET") {
-//         res.send('GET requests are disabled');
-//     } else {
-//         next();
-//     }
-// })
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+});
 
-// app.use((req, res, next) => {
-//     res.status(503).send('The site is under maintenance, please try back soon.');
-// }) 
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+})
+
 
 app.use(express.json());
 app.use(userRouter);
@@ -27,15 +25,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
 
-
-const main = async () => {
-    // const task = await Task.findById('60bb0d2326944e161402be74');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    // const user = await User.findById('60bb0d0526944e161402be72');
-    // await user.populate('tasks').execPopulate();
-    // console.log(user.tasks);
-}
-
-main();
