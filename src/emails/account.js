d3206f8c78f9ed474194ca73a513a1c4833e920f
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-11 23:58:54 +0300

    Created a cancelation email sending function

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,14 +1,28 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendgridAPIKey = 'SG.KzGPIG8kSiucmGFF-NRP3Q.2oXVV4TiR3NHlIbSVQZ4SmlxmRkmvF4lySgFHnScTzA';
+const sendgridAPIKey = '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';
 
 sgMail.setApiKey(sendgridAPIKey);
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'zhek4770@gmail.com',
+        from: 'eugene.maximenko@alisa.in.ua',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'eugene.maximenko@alisa.in.ua',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
 }
