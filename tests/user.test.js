32450ad40b39d5c52a8af476a0ddcb871cc96bfc
tiger3120 <45640139+tiger3120@users.noreply.github.com>
2021-06-13 23:32:46 +0300

    Changed pre test env

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,20 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
 
-jest.setTimeout(10000)
+// Configure the jest
+jest.setTimeout(8000);
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(userOne).save();
+})
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
