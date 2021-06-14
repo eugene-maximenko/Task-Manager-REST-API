31662d734b03c669ff5985f997a0a0ee7ef20f9f
tiger3120 <zhek4770@gmail.com>
2021-06-14 23:58:14 +0300

    New test for authorization

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,20 +1,27 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../src/app');
-const { getMaxListeners } = require('../src/models/user');
 const User = require('../src/models/user');
 
 // Configure the jest
 jest.setTimeout(8000);
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@example.com',
-    password: '56what!!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
     await User.deleteMany();
     await new User(userOne).save();
+    console.log(userOne.tokens[0].token)
 })
 
 test('Should signup a new user', async () => {
@@ -32,9 +39,19 @@ test('Should login existing user', async () => {
     }).expect(200);
 })
 
-test('Should not login nonexistent user', async () => { 
+test('Should not login nonexistent user', async () => {
     await request(app).post('/users/login').send({
         email: userOne.email,
         password: 'thisisnotmypass'
     }).expect(400);
 })
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unathenticated user')
