397a3c843df59575bc19e516c29ccd1d59cae96b
tiger3120 <zhek4770@gmail.com>
2021-06-17 21:15:29 +0300

    Added new test for tasks

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,6 +1,21 @@
 const request = require('supertest');
+const app = require('../src/app');
 const Task = require('../src/models/task');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 
-test('Should create task for user', () => {
+// Configure the jest
+jest.setTimeout(8000);
 
+beforeEach(setupDatabase);
+
+test('Should create task for user', async () => {
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'From my test'
+        })
+        .expect(201);
+
+    
 })
