a4ed59156f944b138ab8e8ad25cdc31a8d2da736
tiger3120 <zhek4770@gmail.com>
2021-07-07 21:51:09 +0300

    Commented out the file

--- src/app.js
+++ src/app.js
@@ -5,10 +5,16 @@ const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+// Create application instance
 const app = express();
 
+// Parse incoming JSON request
 app.use(express.json());
+
+// User users routers
 app.use(userRouter);
+
+// User tasks routers
 app.use(taskRouter);
 
 module.exports = app;
