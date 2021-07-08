45657939f946840ff36b8d501a166a185d322e2a
tiger3120 <zhek4770@gmail.com>
2021-07-08 21:23:43 +0300

    Commented out the file

--- src/routers/task.js
+++ src/routers/task.js
@@ -14,7 +14,7 @@ router.post('/tasks', auth, async (req, res) => {
     });
 
     try {
-        
+
         // Save task
         await task.save();
 
@@ -29,20 +29,24 @@ router.post('/tasks', auth, async (req, res) => {
 // Get all tasks of current user
 router.get('/tasks', auth, async (req, res) => {
 
-    // 
+    // Create objects
     const match = {};
     const sort = {}
 
+    // Check the completed field
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
     }
 
+    // Fill the sort obj
     if (req.query.sortBy) {
         const parts = req.query.sortBy.split(':');
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
     try {
+
+        // Populating data
         await req.user.populate({
             path: 'tasks',
             match,
@@ -52,63 +56,93 @@ router.get('/tasks', auth, async (req, res) => {
                 sort
             }
         }).execPopulate();
+
+        // Send the response
         res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send(e);
     }
 })
 
+// Get a specific task
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     try {
+
+        // Find task
         const task = await Task.findOne({ _id, owner: req.user._id })
 
+        // Validate task
         if (!task) {
             return res.status(404).send();
         }
 
+        // Send the response
         res.send(task);
+
     } catch (e) {
         res.status(500).send(e);
     }
 })
 
+// Update the specific task
 router.patch('/tasks/:id', auth, async (req, res) => {
+
+    // Update fields
     const updates = Object.keys(req.body);
+
+    // Fields allowed to update
     const allowedUpdates = ['description', 'completed'];
+
+    // Validating operation
     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
 
+    // Send error
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' });
     }
 
     try {
+
+        // Find task 
         const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
+        // Send 404 status
         if (!task) {
             return res.status(404).send();
         }
 
+        // Update the fields
         updates.forEach(update => task[update] = req.body[update]);
+
+        // Save a task
         await task.save();
 
+        // Send a response
         res.send(task)
+
     } catch (e) {
         res.status(400).send(e);
     }
 })
 
+// Delete a specific task
 router.delete('/tasks/:id', auth, async (req, res) => {
 
     try {
+
+        // Find task
         const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
+        // Validate task
         if (!task) {
             return res.status(404).send();
         }
 
+        // Send the response
         res.send(task);
+        
     } catch (e) {
         res.status(500).send();
     }
